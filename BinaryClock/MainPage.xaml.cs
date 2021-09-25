b65164fede0b86d3c29274f6e5acba5d22d36be2
Andrey Zakharov <mediaexplorer74@hotmail.com>
2021-09-25 13:38:11 +0300

    .

---
+++ BinaryClock/MainPage.xaml.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading.Tasks;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI;
+using Windows.UI.Core;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using BinaryClock.Converter;
+using BinaryClock.ViewModels;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace BinaryClock
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        readonly MainViewModel viewModel = new MainViewModel();
+        readonly Task colorUpdateTask;
+        readonly DispatcherTimer clock;
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+            this.DataContext = viewModel;
+            colorUpdateTask = BeginColorUpdate(viewModel);
+            clock = CreateClock();
+        }
+
+        private DispatcherTimer CreateClock()
+        {
+            var clock = new DispatcherTimer();
+            clock.Interval = TimeSpan.FromSeconds(1);
+            clock.Tick += (o, e) => {
+                viewModel.Time = DateTime.Now;
+            };
+            clock.Start();
+            return clock;
+        }
+
+        private Task BeginColorUpdate(MainViewModel viewModel)
+        {
+            return Task.Run(async () =>
+            {
+                int iterations = 0;
+                while (true)
+                {
+                    await Task.Delay(50);
+                    await Dispatcher.RunAsync(CoreDispatcherPriority.Normal, () =>
+                    {
+                        viewModel.BackgroundColor = Color.FromArgb(255,
+                            (byte)(128 + (int)(Math.Sin(iterations * .01f) * 127)),
+                            (byte)(128 + (int)(Math.Sin(iterations * .0125f + 1) * 127)),
+                            (byte)(128 + (int)(Math.Sin(iterations * .009f + 1) * 127)));
+                    });
+                    iterations++;
+                }
+            });
+        }
+    }
+}
---
+++ BinaryClock/ViewModels/MainViewModel.cs
@@ -0,0 +1,55 @@
+﻿using BinaryClock.Converter;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI;
+using Windows.UI.Xaml.Media;
+
+namespace BinaryClock.ViewModels
+{
+    public class MainViewModel : INotifyPropertyChanged
+    {
+        private Color backgroundColor = Color.FromArgb(255, 0, 0, 255);
+
+        public Color BackgroundColor {
+            get { return backgroundColor; }
+            set {
+                if (backgroundColor != value)
+                {
+                    backgroundColor = value;
+                    if (PropertyChanged != null)
+                    {
+                        PropertyChanged.Invoke(this, new PropertyChangedEventArgs(nameof(BackgroundColor)));
+                        PropertyChanged.Invoke(this, new PropertyChangedEventArgs(nameof(BackgroundBrush)));
+                    }
+                }
+            }
+        }
+
+        public SolidColorBrush BackgroundBrush
+        {
+            get {
+                return new SolidColorBrush(BackgroundColor);
+            }
+        }
+
+        private DateTime time;
+
+        public DateTime Time
+        {
+            get { return time; }
+            set {
+                if (time != value)
+                {
+                    time = value;
+                    PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(Time)));
+                }
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
